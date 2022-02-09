5fb33aa0de1cf873dd964f873c4598e693f5c919
amiribnjaman <amiribnjaman@gmail.com>
2022-02-09 16:16:27 +0600

    event section almost completed

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 let inputField = document.getElementById('input-field')
 let submitBtn = document.getElementById('submit-btn')
 let tBody = document.getElementById('table-body')
+
+let count = 0
+
+submitBtn.addEventListener('click', function(){
+    if(inputField.value == ''){
+        alert('Please, provide a valid text')
+    } else {
+        let newRow = document.createElement('tr')
+
+        count++
+        newRow.innerHTML = `
+            <td>${count}</td>
+            <td>${inputField.value}</td>
+            <td>
+                <button class="btn btn-danger">Delete</button>
+            </td>
+        `
+
+        tBody.appendChild(newRow)
+
+    }
+})
